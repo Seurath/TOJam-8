642fd619d2b9fffca1a538bd2d8b9405c238726b
jacqueswaller <jacqueswaller@hotmail.com>
2013-05-04 19:28:48 -0400

    I think I have the punch man working.

--- tojam/Assets/Scripts/PunchPlayer.cs
+++ tojam/Assets/Scripts/PunchPlayer.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Player : MonoBehaviour {
+public class PunchPlayer : MonoBehaviour {
 	public Vector2 joystickLeft;
 	public Vector2 joystickRight;
 	
 	public float moveSpeed = 4.0f;
 	public float backwardsMultiplier = 0.5f;
 	public float strafeMultiplier = 0.75f;
+	
+	public float turnSpeed = 4.0f;
 	
 	private CharacterController cc;
 
@@ -34,6 +36,8 @@ public class Player : MonoBehaviour {
 		
 		Vector3 localMovementSpeed = new Vector3(xMovement, 0.0f, zMovement);
 		cc.SimpleMove(transform.TransformDirection(localMovementSpeed));
+		
+		transform.Rotate (0.0f, joystickRight.x * turnSpeed, 0.0f);
 	}
 	
 	/*
