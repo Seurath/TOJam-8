b38140c725add2b3d1707fd507aa7fd7a6c0228c
Mark Cautillo <mark.cautillo@gmail.com>
2013-05-05 01:21:10 -0400

    * Drone motion is more interesting. * Drones now shoot at you.

--- tojam/Assets/src/drone/Drone.cs
+++ tojam/Assets/src/drone/Drone.cs
@@ -1,40 +1,60 @@
 using UnityEngine;
 using System.Collections;
 
-public class Drone : MonoBehaviour {
+public class Drone : MonoBehaviour
+{
 	
 	public GameObject target;
 	public float rotationRate;
 	public float speed;
 	public float distanceCutoffNear;
 	public float distanceCutoffFar;
-	
+	public GameObject[] bulletSpawns;
+	public GameObject bullet;
 	public float distance;
 	// Use this for initialization
-	void Start () {
-		target = GameObject.Find("fighter");
+	void Start ()
+	{
+		target = GameObject.Find ("fighter");
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-		
-		
-		 distance = Vector3.Distance(this.transform.position, target.transform.position);
+	private float timeUntilNextShot = 0.25f;
+	private int currentBulletSpawn = 0;
 	
-		if (distance > distanceCutoffNear || distance < distanceCutoffFar )
-		{
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		timeUntilNextShot -= Time.deltaTime;
+		if (distance < distanceCutoffFar) {
+			if (timeUntilNextShot < 0f) {
+				timeUntilNextShot = Random.Range (2f, 2.5f);
+				GameObject.Instantiate (bullet, bulletSpawns [currentBulletSpawn].transform.position, bulletSpawns [currentBulletSpawn].transform.rotation);
+				currentBulletSpawn ++;
+				currentBulletSpawn = currentBulletSpawn % bulletSpawns.Length;
+			}
+		}
+		
+		if (target == null) {
+			return;	
+		}
+		
+		distance = Vector3.Distance (this.transform.position, target.transform.position);
+	
+		Vector3 targetDirection = this.target.transform.position - this.transform.position;
+		Quaternion targetAngle = Quaternion.LookRotation (targetDirection);
+		
+		Quaternion resultDirection = Quaternion.RotateTowards (this.transform.rotation, targetAngle, Time.deltaTime * rotationRate);
+		
+		
+		
+		this.transform.rotation = resultDirection;
+		
+		
+		if (distance > distanceCutoffNear && distance < distanceCutoffFar) {
 			
-			Vector3 targetDirection =  this.target.transform.position - this.transform.position;
-			Quaternion targetAngle = Quaternion.LookRotation(targetDirection);
-		
-			Quaternion resultDirection = Quaternion.RotateTowards(this.transform.rotation, targetAngle, Time.deltaTime * rotationRate);
-		
-		
-		
-			this.transform.rotation = resultDirection;
-			
-			//this.rigidbody.AddRelativeForce(Vector3.forward * speed * Time.deltaTime);
+				
+			this.rigidbody.AddRelativeForce (Vector3.forward * speed * Time.deltaTime);
 		}
 		
 		
